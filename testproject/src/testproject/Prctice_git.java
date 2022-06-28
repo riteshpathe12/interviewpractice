1b30069b7033aaafdd70bd7ddd9ab0c458aaa067
riteshpathe12 <106052553+riteshpathe12@users.noreply.github.com>
2022-06-28 12:15:29 +0530

    my commit
    My first task is completed please check it

---
+++ testproject/src/testproject/Prctice_git.java
@@ -0,0 +1,11 @@
+package testproject;
+
+public class Prctice_git {
+
+	public static void main(String[] args) {
+		System.out.println("Hellio my world");
+         System.out.println("practiceof interview");
+         System.out.println("uhdsuhidshiughuisdh");
+	}
+
+}
